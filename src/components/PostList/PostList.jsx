58efbf6ea73d86fc1b098ff12bd945ae34435445
KanekarSnehal <snehalkanekar43@gmail.com>
2022-05-16 00:54:21 +0530

    added postlist component

---
+++ src/components/PostList/PostList.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { SinglePost } from "../index";
+
+export const PostList = () => {
+  return (
+    <div className="flex flex-col gap-5">
+      <SinglePost />
+    </div>
+  );
+};

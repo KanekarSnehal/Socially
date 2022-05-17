7427f2a907f58fe862c42963e21484dd9716c73b
KanekarSnehal <snehalkanekar43@gmail.com>
2022-05-17 15:43:55 +0530

    added token verification

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,20 +1,34 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { AuthPage, Home } from "../page";
+import { AuthPage, Home } from "../features";
 import { MainContainer } from "../components";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+export const ProtectedRoute = () => {
+  const { token } = useSelector((state) => state.auth);
+  const location = useLocation();
+  return token ? (
+    <Outlet />
+  ) : (
+    <Navigate to={"/"} state={{ from: location?.pathname }} replace />
+  );
+};
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<AuthPage />} />
-      <Route
-        path="/home"
-        element={
-          <MainContainer>
-            <Home />
-          </MainContainer>
-        }
-      />
+      <Route element={<ProtectedRoute />}>
+        <Route
+          path="/home"
+          element={
+            <MainContainer>
+              <Home />
+            </MainContainer>
+          }
+        />
+      </Route>
     </Routes>
   );
 };

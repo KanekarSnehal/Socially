bbbc21d65d7eb970fe9779f70da41a7be96a15e7
KanekarSnehal <snehalkanekar43@gmail.com>
2022-05-17 15:45:16 +0530

    installed tostify

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import { AppRoutes } from "./routes/AppRoutes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="App">
+      <ToastContainer autoClose={2000} theme="colored" />
       <AppRoutes />
     </div>
   );

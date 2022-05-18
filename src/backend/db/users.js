cc8b5a753de1381b3e9acc57bf9a70c011deb46b
KanekarSnehal <snehalkanekar43@gmail.com>
2022-05-19 05:25:11 +0530

    added users to db

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -11,6 +11,9 @@ export const users = [
     fullName: "Adarsh Balika",
     username: "adarshbalika",
     password: "adarshBalika123",
+    bio: "this is my bio",
+    profileImage:
+      "https://res.cloudinary.com/dflebgpde/image/upload/v1652528175/socially/hacker_obpy2s.png",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
@@ -19,6 +22,88 @@ export const users = [
     fullName: "Snehal Kanekar",
     username: "snehalkanekar",
     password: "snehal@123",
+    bio: "this is my bio",
+    profileImage:
+      "https://res.cloudinary.com/dflebgpde/image/upload/v1652866709/socially/IMG_20190404_181111-min_tjvnvy.jpg",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    fullName: "Vedant Kanekar",
+    username: "vedantkanekar",
+    password: "vednat@123",
+    bio: "this is my bio",
+    profileImage:
+      "https://res.cloudinary.com/dflebgpde/image/upload/v1652867848/socially/profile_1_tpbsuk.png",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+
+  {
+    _id: uuid(),
+    fullName: "Sukanya Sen",
+    username: "sukanyasen",
+    password: "sukanya@123",
+    bio: "this is my bio",
+    profileImage:
+      "https://res.cloudinary.com/dflebgpde/image/upload/v1652867247/socially/profile_hcosib.png",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    fullName: "Prabhav Sharma",
+    username: "prabhavsharma",
+    password: "prabhav@123",
+    bio: "this is my bio",
+    profileImage:
+      "https://res.cloudinary.com/dflebgpde/image/upload/v1652867848/socially/profile_1_tpbsuk.png",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    fullName: "Virat Kholi",
+    username: "imVkholi",
+    password: "virat@123",
+    bio: "A proud husband and father ❤️",
+    profileImage:
+      "https://res.cloudinary.com/dflebgpde/image/upload/v1652868350/socially/bKUpXvlc_400x400_grtnm6.jpg",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+
+  {
+    _id: uuid(),
+    fullName: "Sharath Nair",
+    username: "sharathnair",
+    password: "sharath@123",
+    bio: "this is my bio",
+    profileImage:
+      "https://res.cloudinary.com/dflebgpde/image/upload/v1652867848/socially/profile_1_tpbsuk.png",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    fullName: "Prathmesh Jagtap",
+    username: "prathmesh",
+    password: "prathmesh@123",
+    bio: "this is my bio",
+    profileImage:
+      "https://res.cloudinary.com/dflebgpde/image/upload/v1652867848/socially/profile_1_tpbsuk.png",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    fullName: "Leena Kanekar",
+    username: "leena123",
+    password: "leena@123",
+    bio: "this is my bio",
+    profileImage:
+      "https://res.cloudinary.com/dflebgpde/image/upload/v1652867247/socially/profile_hcosib.png",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },

582afd7fc75beece1205dede23e6edf163ec780b
KanekarSnehal <snehalkanekar43@gmail.com>
2022-05-19 19:30:54 +0530

    made changes in the backend code

--- src/backend/controllers/PostController.js
+++ src/backend/controllers/PostController.js
@@ -155,6 +155,7 @@ export const editPostHandler = function (schema, request) {
 
 export const likePostHandler = function (schema, request) {
   const user = requiresAuth.call(this, request);
+
   try {
     if (!user) {
       return new Response(
@@ -176,11 +177,11 @@ export const likePostHandler = function (schema, request) {
         { errors: ["Cannot like a post that is already liked. "] }
       );
     }
-    post.likes.dislikedBy = post.likes.dislikedBy.filter(
-      (currUser) => currUser._id !== user._id
-    );
+    // post.likes.dislikedBy = post.likes.dislikedBy.filter(
+    //   (currUser) => currUser._id !== user._id
+    // );
     post.likes.likeCount += 1;
-    post.likes.likedBy.push(user);
+    post.likes.likedBy.push({ _id: user._id, username: user.username });
     this.db.posts.update({ _id: postId }, { ...post, updatedAt: formatDate() });
     return new Response(201, {}, { posts: this.db.posts });
   } catch (error) {
@@ -222,18 +223,19 @@ export const dislikePostHandler = function (schema, request) {
         { errors: ["Cannot decrement like less than 0."] }
       );
     }
-    if (post.likes.dislikedBy.some((currUser) => currUser._id === user._id)) {
-      return new Response(
-        400,
-        {},
-        { errors: ["Cannot dislike a post that is already disliked. "] }
-      );
-    }
+    // if (post.likes.dislikedBy.some((currUser) => currUser._id === user._id)) {
+    //   return new Response(
+    //     400,
+    //     {},
+    //     { errors: ["Cannot dislike a post that is already disliked. "] }
+    //   );
+    // }
     post.likes.likeCount -= 1;
     const updatedLikedBy = post.likes.likedBy.filter(
       (currUser) => currUser._id !== user._id
     );
-    post.likes.dislikedBy.push(user);
+    // post.likes.dislikedBy.push(user);
+
     post = { ...post, likes: { ...post.likes, likedBy: updatedLikedBy } };
     this.db.posts.update({ _id: postId }, { ...post, updatedAt: formatDate() });
     return new Response(201, {}, { posts: this.db.posts });

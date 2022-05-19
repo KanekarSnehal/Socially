3f683fef9d8b09497b27d1ed80c1d2004bfbaf16
KanekarSnehal <snehalkanekar43@gmail.com>
2022-05-19 05:32:01 +0530

    added api calls

---
+++ src/services/post.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { postUrl, config } from "../utils/index";
+
+// get all posts
+export const getAllPosts = () => axios.get(postUrl);
+
+// get single post by postId
+export const getPostByPostId = (postId) => axios.get(`${postUrl}/${postId}`);
+
+// get all posts by username
+export const getPostsByUserName = (username) =>
+  axios.get(`${postUrl}/${username}`);
+
+//   add new post to user's db
+export const addPost = (postData) => {
+  return axios.post(postUrl, { postData }, config);
+};
+
+// delete a post from user's db
+export const deletePost = (postId) =>
+  axios.delete(`${postUrl}/${postId}`, config);
+
+// edit a post from user's db
+export const editPost = (postData) =>
+  axios.post(`${postUrl}/edit/${postData._id}`, { postData }, config);

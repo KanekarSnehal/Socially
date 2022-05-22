2b0679ad5defff34f319b2400166f86f33f1a753
KanekarSnehal <snehalkanekar43@gmail.com>
2022-05-22 13:30:38 +0530

    Added follow button component

---
+++ src/components/FollowButton/FollowButton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const FollowButton = () => {
+  return (
+    <button className="font-semibold cursor-pointer text-white bg-secondary-300 rounded-2xl px-3 py-1  h-8 hover:bg-secondary-400">
+      + Follow
+    </button>
+  );
+};

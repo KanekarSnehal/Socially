188986728e237ab93c09abe2871895e1bb31a62e
KanekarSnehal <snehalkanekar43@gmail.com>
2022-05-22 13:32:58 +0530

    added exports

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,4 @@ export { Home } from "./Home/Home";
 export { Explore } from "./Explore/Explore";
 export { Bookmark } from "./Bookmark/Bookmark";
 export { Profile } from "./Profile/Profile";
+export { UserProfile } from "./UserProfile/UserProfile";

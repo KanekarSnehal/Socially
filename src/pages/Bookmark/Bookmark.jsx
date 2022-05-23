ba79b3b9de4f164d8afa1916ab225ba08c5fe90a
KanekarSnehal <snehalkanekar43@gmail.com>
2022-05-23 20:24:23 +0530

    fixed bookmark issue

--- src/pages/Bookmark/Bookmark.jsx
+++ src/pages/Bookmark/Bookmark.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { PostList } from "../../components";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchAllBookmarks } from "../../app/features/postSlice";
 
 export const Bookmark = () => {
   const { bookmarks } = useSelector((state) => state.post);
+  const dispatch = useDispatch();
+  const { allPosts } = useSelector((state) => state.post);
+
+  useEffect(() => {
+    dispatch(fetchAllBookmarks());
+  }, [allPosts]);
+
+  const bookmarkPost = allPosts?.filter((post) =>
+    bookmarks.find((bookmarkPost) => bookmarkPost._id === post._id)
+  );
+
   return (
     <div className="lg:w-1/2 w-full px-4 py-2 relative">
       <p className="font-semibold text-2xl text-secondary-300 mb-8">Bookmark</p>
-      <PostList posts={bookmarks} />
+      <PostList posts={bookmarkPost} />
     </div>
   );
 };

ead45e8418e42d46971b10bd4feadb483ab6b233
KanekarSnehal <snehalkanekar43@gmail.com>
2022-05-24 03:52:45 +0530

    added api calls in redux

--- src/app/features/postSlice.js
+++ src/app/features/postSlice.js
@@ -13,17 +13,29 @@ import {
   getBookmarks,
   unBookmarkPost,
 } from "../../services/user";
+import {
+  addComment,
+  getComments,
+  editComment,
+  deleteComment,
+  likeComment,
+  dislikeComment,
+} from "../../services/comment";
+import { toast } from "react-toastify";
 
 const initialState = {
   allPosts: [],
   userPosts: [],
   bookmarks: [],
+  comments: [],
   bookmarkStatus: "idle",
   getAllPostsStatus: "idle",
   getUserPostsStatus: "idle",
   postAddStatus: "idle",
   postDeleteStatus: "idle",
   postEditStatus: "idle",
+  commentsStatus: "idle",
+  commentsError: null,
 };
 
 export const fetchAllPosts = createAsyncThunk(
@@ -125,6 +137,78 @@ export const bookmarkUnbookmarkUserPost = createAsyncThunk(
   }
 );
 
+export const fetchComments = createAsyncThunk(
+  "comment/getComments",
+  async (postId, { rejectWithValue }) => {
+    try {
+      const { data } = await getComments(postId);
+      return data.comments;
+    } catch (e) {
+      return rejectWithValue(e.message);
+    }
+  }
+);
+
+export const addUserComment = createAsyncThunk(
+  "comments/addComment",
+  async ({ postId, commentData }, { rejectWithValue }) => {
+    try {
+      const { data } = await addComment(postId, commentData);
+      return data.posts;
+    } catch (e) {
+      return rejectWithValue(e.message);
+    }
+  }
+);
+
+export const editUserComment = createAsyncThunk(
+  "comments/editComment",
+  async ({ postId, commentId, commentData }, { rejectWithValue }) => {
+    try {
+      const { data } = await editComment(postId, commentId, commentData);
+      return data.posts;
+    } catch (e) {
+      return rejectWithValue(e.message);
+    }
+  }
+);
+
+export const deleteUserComment = createAsyncThunk(
+  "comments/deleteComment",
+  async ({ postId, commentId }, { rejectWithValue }) => {
+    try {
+      const { data } = await deleteComment(postId, commentId);
+      return data.posts;
+    } catch (e) {
+      return rejectWithValue(e.message);
+    }
+  }
+);
+
+export const likeUserComment = createAsyncThunk(
+  "comments/likeComment",
+  async ({ postId, commentId }, { rejectWithValue }) => {
+    try {
+      const { data } = await likeComment(postId, commentId);
+      return data.posts;
+    } catch (e) {
+      return rejectWithValue(e.message);
+    }
+  }
+);
+
+export const dislikeUserComment = createAsyncThunk(
+  "comments/dislikeComment",
+  async ({ postId, commentId }, { rejectWithValue }) => {
+    try {
+      const { data } = await dislikeComment(postId, commentId);
+      return data.posts;
+    } catch (e) {
+      return rejectWithValue(e.message);
+    }
+  }
+);
+
 const postSlice = createSlice({
   name: "post",
   initialState,
@@ -211,6 +295,90 @@ const postSlice = createSlice({
       })
       .addCase(bookmarkUnbookmarkUserPost.rejected, (state) => {
         state.bookmarkStatus = "rejected";
+      })
+      .addCase(fetchComments.pending, (state) => {
+        state.commentsStatus = "pending";
+      })
+      .addCase(fetchComments.fulfilled, (state, action) => {
+        state.commentsStatus = "fulfilled";
+        state.comments = action.payload;
+      })
+      .addCase(fetchComments.rejected, (state, action) => {
+        state.commentsStatus = "rejected";
+        state.commentsError = action.payload;
+        toast.error(
+          `Some went wrong, Please try again, ${state.commentsError}`
+        );
+      })
+      .addCase(addUserComment.pending, (state) => {
+        state.commentsStatus = "pending";
+      })
+      .addCase(addUserComment.fulfilled, (state, action) => {
+        state.commentsStatus = "fulfilled";
+        state.allPosts = action.payload;
+      })
+      .addCase(addUserComment.rejected, (state, action) => {
+        state.commentsStatus = "rejected";
+        state.commentsError = action.payload;
+        toast.error(
+          `Some went wrong, Please try again, ${state.commentsError}`
+        );
+      })
+      .addCase(editUserComment.pending, (state) => {
+        state.commentsStatus = "pending";
+      })
+      .addCase(editUserComment.fulfilled, (state, action) => {
+        state.commentsStatus = "fulfilled";
+        state.allPosts = action.payload;
+      })
+      .addCase(editUserComment.rejected, (state, action) => {
+        state.commentsStatus = "rejected";
+        state.commentsError = action.payload;
+        toast.error(
+          `Some went wrong, Please try again, ${state.commentsError}`
+        );
+      })
+      .addCase(deleteUserComment.pending, (state) => {
+        state.commentsStatus = "pending";
+      })
+      .addCase(deleteUserComment.fulfilled, (state, action) => {
+        state.commentsStatus = "fulfilled";
+        state.allPosts = action.payload;
+      })
+      .addCase(deleteUserComment.rejected, (state, action) => {
+        state.commentsStatus = "rejected";
+        state.commentsError = action.payload;
+        toast.error(
+          `Some went wrong, Please try again, ${state.commentsError}`
+        );
+      })
+      .addCase(likeUserComment.pending, (state) => {
+        state.commentsStatus = "pending";
+      })
+      .addCase(likeUserComment.fulfilled, (state, action) => {
+        state.commentsStatus = "fulfilled";
+        state.allPosts = action.payload;
+      })
+      .addCase(likeUserComment.rejected, (state, action) => {
+        state.commentsStatus = "rejected";
+        state.commentsError = action.payload;
+        toast.error(
+          `Some went wrong, Please try again, ${state.commentsError}`
+        );
+      })
+      .addCase(dislikeUserComment.pending, (state) => {
+        state.commentsStatus = "pending";
+      })
+      .addCase(dislikeUserComment.fulfilled, (state, action) => {
+        state.commentsStatus = "fulfilled";
+        state.allPosts = action.payload;
+      })
+      .addCase(dislikeUserComment.rejected, (state, action) => {
+        state.commentsStatus = "rejected";
+        state.commentsError = action.payload;
+        toast.error(
+          `Some went wrong, Please try again, ${state.commentsError}`
+        );
       });
   },
 });

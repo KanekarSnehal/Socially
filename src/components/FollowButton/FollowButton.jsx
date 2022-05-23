7954ce1cff563fa4b7c99d5b91d8906223bb1caf
KanekarSnehal <snehalkanekar43@gmail.com>
2022-05-23 17:38:02 +0530

    Added follow button functionality

--- src/components/FollowButton/FollowButton.jsx
+++ src/components/FollowButton/FollowButton.jsx
@@ -1,8 +1,42 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { followUnfollowUser } from "../../app/features/userSlice";
 
-export const FollowButton = () => {
-  return (
-    <button className="font-semibold cursor-pointer text-white bg-secondary-300 rounded-2xl px-3 py-1  h-8 hover:bg-secondary-400">
+export const FollowButton = ({ userDetails }) => {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+  const isFollowing = userDetails.followers.some(
+    (currentUser) => currentUser.username === user.username
+  );
+
+  return isFollowing ? (
+    <button
+      className="font-semibold cursor-pointer text-secondary-400 bg-secondary-100 rounded-2xl px-3 py-1  h-8 hover:bg-secondary-200"
+      onClick={() =>
+        dispatch(
+          followUnfollowUser({
+            userId: userDetails._id,
+            isFollowing: isFollowing,
+            dispatch,
+          })
+        )
+      }
+    >
+      Following
+    </button>
+  ) : (
+    <button
+      className="font-semibold cursor-pointer text-white bg-secondary-300 rounded-2xl px-3 py-1  h-8 hover:bg-secondary-400"
+      onClick={() =>
+        dispatch(
+          followUnfollowUser({
+            userId: userDetails._id,
+            isFollowing: isFollowing,
+            dispatch,
+          })
+        )
+      }
+    >
       + Follow
     </button>
   );

e4be08407d3f0f7b448f3d2b7ca4efd80855f8b5
KanekarSnehal <snehalkanekar43@gmail.com>
2022-06-04 02:46:49 +0530

    added loaders

--- src/pages/Bookmark/Bookmark.jsx
+++ src/pages/Bookmark/Bookmark.jsx
@@ -2,9 +2,10 @@ import React, { useEffect } from "react";
 import { PostList } from "../../components";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchAllBookmarks } from "../../app/features/postSlice";
+import loader from "../../assets/loader.svg";
 
 export const Bookmark = () => {
-  const { bookmarks } = useSelector((state) => state.post);
+  const { bookmarks, bookmarkStatus } = useSelector((state) => state.post);
   const dispatch = useDispatch();
   const { allPosts } = useSelector((state) => state.post);
 
@@ -18,9 +19,13 @@ export const Bookmark = () => {
 
   return (
     <div className="lg:w-1/2 w-full px-4 py-2 relative">
-      <p className="font-semibold text-2xl text-secondary-300 mb-8">Bookmark</p>
+      <p className="font-semibold text-2xl text-secondary-300">Bookmark</p>
       {bookmarkPost.length !== 0 ? (
         <PostList posts={bookmarkPost} />
+      ) : bookmarkStatus === "pending" ? (
+        <div className="flex justify-center items-center">
+          <img src={loader} className="w-36 h-36" alt="loader" />
+        </div>
       ) : (
         <p className="text-secondary-300 text-2xl text-center font-semibold">
           Bookmark posts to see them here!
